1ad988279331fd4293b6cb3221ad08fba5334b81
Niklas Zbozinek <nz@delodi.net>
2022-09-19 12:08:52 +0200

    added todo to readme startet grouping

--- 592e9341-c3ba-451c-bb1a-8241f78ea501/src/app/backend.service.ts
+++ 592e9341-c3ba-451c-bb1a-8241f78ea501/src/app/backend.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { retry, catchError, map } from 'rxjs/operators';
+import { retry, catchError, map, groupBy } from 'rxjs/operators';
 
 import { apiUrl } from './config.service';
 import { Event } from '../models/event';
@@ -10,13 +10,9 @@ import { Event } from '../models/event';
   providedIn: 'root',
 })
 export class BackendService {
-  constructor(private http: HttpClient) {}
+  public resultData = [];
 
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-    }),
-  };
+  constructor(private http: HttpClient) {}
 
   //HTTP-Client API get() method => Fetch event list
   getAllEvents(): Observable<Event[]> {
@@ -26,6 +22,16 @@ export class BackendService {
       .pipe(
         map((results) => results.sort((e1, e2) => (e1.date < e2.date ? -1 : 1)))
       );
+  }
+
+  groupBy(events: Event[]) {
+    let data = new Set(events.map((item) => new Date(item.date)));
+    data.forEach((date) => {
+      this.resultData.push({
+        date: date,
+        products: events.filter((i) => new Date(i.date) === date),
+      });
+    });
   }
 
   //Error Handling
--- README.md
+++ README.md
@@ -34,7 +34,7 @@ event shop
    - feature_us1/00_event-view: Single Event View, boilerplate
    - feature_us1/01_event_list: List of single event Views, read data, create dataobjects, pass into event view component
    - feature_us1/02_sort-event-list: Sort List of single event views ASC/DESC options
-   - feature_us1/03_sticky-date: Stick date to top in event list whil scrolling
+   - feature_us1/03_sticky-date: Stick date to top in event list while scrolling: Group Event list first
    - feature_us1/04_link-google-maps: Klicking on location opens a new browser tab with google maps location
 
    - feature_us1/05_preload-event-pictures
@@ -47,8 +47,13 @@ event shop
    -feature_us2/02_filter-city: filter events by town (All in London) anyway
 
 3. A User can add events to shopping cart
+
    - feature_us3/00_shoppping-cart-view: When clicking on the shopping cart icon all shopped events will be listed, grey /. not clickable if no events listed yet
    - feature_us3/01_add-item: When clicking on add item in single event view item will be 1. added to shopping cart and 2. delted from list
+
+4. Addional code developments
+   - Picture formation: Zoom in flyer if not protrait?
+   - Show date if no start and end date, use ngif?
 
 ### Addiotional Ideas
 
